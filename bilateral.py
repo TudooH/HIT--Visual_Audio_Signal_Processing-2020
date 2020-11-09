b15345acfae3cd9d7f1c729c5e248195ffe7d514
TudooH <1014090526@qq.com>
2020-11-09 22:42:21 +0800

    add bilateral

---
+++ bilateral.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+import math
+
+
+def bilateral_filter(channel, size, sigma_d, sigma_r):
+    mid = int(size / 2)
+    w_d = np.array([[math.exp(-((i - mid) ** 2 + (j - mid) ** 2) / (2 * sigma_d ** 2))
+                     for j in range(size)] for i in range(size)])
+
+    channel = channel.astype(np.float)
+    out = np.zeros((channel.shape[0] - size + 1, channel.shape[1] - size + 1), dtype=np.uint8)
+    for i in range(channel.shape[0] - size + 1):
+        for j in range(channel.shape[1] - size + 1):
+            w_r = np.array([[math.exp(-(channel[i + ii][j + jj] - channel[i + mid][j + mid]) ** 2 / (2 * sigma_r ** 2))
+                            for jj in range(size)] for ii in range(size)])
+            w = w_d * w_r
+            w = w / sum(sum(w))
+            out[i][j] = sum(sum(w * channel[i: i+size, j: j+size]))
+    return out
+
+
+if __name__ == '__main__':
+    img = cv2.imread('img/lena.tiff')
+    channels = cv2.split(img)
+
+    Size = 5
+    img_ = np.zeros((img.shape[0] - Size + 1, img.shape[1] - Size + 1, 3), dtype=np.uint8)
+    for c in range(3):
+        print('start channel {}'.format(c))
+        img_[:, :, c] = bilateral_filter(channels[c], Size, 5, 30)
+        print('finished')
+
+    cv2.imwrite('img/bilateral/bilateral.png', img_)
+    cv2.imshow('origin', img)
+    cv2.imshow('img', img_)
+    cv2.waitKey(0)
--- homomorphic.py
+++ homomorphic.py
@@ -10,21 +10,21 @@ def homomorphic_filter(src, d0=10, r1=0.5, rh=2, c=4, h=2.0, l=0.5):
     rows, cols = gray.shape
 
     gray_fft = np.fft.fft2(gray)
-    gray_fftshift = np.fft.fftshift(gray_fft)
-    dst_fftshift = np.zeros_like(gray_fftshift)
+    gray_fft_shift = np.fft.fftshift(gray_fft)
     M, N = np.meshgrid(np.arange(-cols // 2, cols // 2), np.arange(-rows // 2, rows // 2))
     D = np.sqrt(M ** 2 + N ** 2)
     Z = (rh - r1) * (1 - np.exp(-c * (D ** 2 / d0 ** 2))) + r1
-    dst_fftshift = Z * gray_fftshift
-    dst_fftshift = (h - l) * dst_fftshift + l
-    dst_ifftshift = np.fft.ifftshift(dst_fftshift)
-    dst_ifft = np.fft.ifft2(dst_ifftshift)
-    dst = np.real(dst_ifft)
+    dst_fft_shift = Z * gray_fft_shift
+    dst_fft_shift = (h - l) * dst_fft_shift + l
+    dst_shift = np.fft.ifftshift(dst_fft_shift)
+    dst = np.fft.ifft2(dst_shift)
+    dst = np.real(dst)
     dst = np.uint8(np.clip(dst, 0, 255))
     return dst
 
 
 img = cv2.imread('img/lena.tiff')
-img = homomorphic_filter(img)
+for i in range(3):
+    img[:, :, i] = homomorphic_filter(img[:, :, i])
 cv2.imshow('img', img)
 cv2.waitKey(0)
